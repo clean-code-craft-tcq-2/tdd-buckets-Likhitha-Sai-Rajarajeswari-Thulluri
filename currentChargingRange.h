d55f93b5544e9f168e76855893d0f55fca0734ea
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-03-16 10:11:59 +0530

    Update currentChargingRange.h

--- currentChargingRange.h
+++ currentChargingRange.h
@@ -3,4 +3,8 @@
 #include<string>
 #include<vector>
 
+std::map<std::string, int> populateCurrentRange(std::vector<int> vecCurrentSamples);
+
+void printOnConsole(std::map<std::string, int> mapCurrentRange);
+
 std::map<std::string, int> inferCurrentChargingRange(std::vector<int> vecCurrentSamples);
