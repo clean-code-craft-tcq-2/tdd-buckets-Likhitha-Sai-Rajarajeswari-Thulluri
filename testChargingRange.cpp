63bd89b5d1e66bb6b8277dba81f9ac3f3025b3ae
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-03-16 10:32:58 +0530

    Update testChargingRange.cpp

--- testChargingRange.cpp
+++ testChargingRange.cpp
@@ -3,8 +3,14 @@
 #include "test/catch.hpp"
 #include "currentChargingRange.h"
 
-TEST_CASE("detect the current charging range and output the number of readings") {
+TEST_CASE("detect sorted single current charging range and output the number of readings") {
   std::vector<int> chargingRange = {4, 5};
   std::map<std::string, int> rangeFrequency = {{"4-5", 2}};
+  REQUIRE(inferCurrentChargingRange(chargingRange) == rangeFrequency);
+}
+
+TEST_CASE("detect sorted multiple current charging range and output the number of readings") {
+  std::vector<int> chargingRange = {4, 5, 6, 7,8};
+  std::map<std::string, int> rangeFrequency = {{"4-5", 2}, {"6-8", 3}};
   REQUIRE(inferCurrentChargingRange(chargingRange) == rangeFrequency);
 }
