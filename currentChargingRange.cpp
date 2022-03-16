8812c3ccaf76d272b5b51ebcfeaa6e0325a4d658
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-03-16 10:17:39 +0530

    Update currentChargingRange.cpp

--- currentChargingRange.cpp
+++ currentChargingRange.cpp
@@ -5,35 +5,27 @@ std::map<std::string, int> populateCurrentRange(std::vector<int> vecCurrentSampl
 	int length = 1;
 	std::map<std::string, int> mapCurrentRange;
 	std::sort(vecCurrentSamples.begin(), vecCurrentSamples.end());
-    for(int i = 1; i <= vecCurrentSamples.size(); i++)
-    {
-        if (i == vecCurrentSamples.size() || vecCurrentSamples[i] - vecCurrentSamples[i - 1] != 1)
-        {
-            if (length == 1)
-            {
-                mapCurrentRange[to_string(vecCurrentSamples[i - length])] = length;
-            }
-            else
-            {
-                string temp = std::to_string(vecCurrentSamples[i - length]) +
-                            "-" + std::to_string(vecCurrentSamples[i - 1]);
-                mapCurrentRange[temp] = length;
-            }
-            length = 1;
-        }
-        else
-        {
-            length++;
-        }
-    }
-    return mapCurrentRange;
+	for(int i = 1; i <= vecCurrentSamples.size(); i++)
+	{
+		if (i == vecCurrentSamples.size() || vecCurrentSamples[i] - vecCurrentSamples[i - 1] != 1)
+		{
+		    std::string temp = std::to_string(vecCurrentSamples[i - length]) +
+				    "-" + std::to_string(vecCurrentSamples[i - 1]);
+		    mapCurrentRange[temp] = length;
+		    length = 1;
+		}
+		else
+		{
+		    length++;
+		}
+	}
+	return mapCurrentRange;
 }
 
 std::map<std::string, int> inferCurrentChargingRange(std::vector<int> vecCurrentSamples) {
 	std::map<std::string, int> mapCurrentRange = populateCurrentRange(vecCurrentSamples);;
 	return mapCurrentRange;
 }
-
 
 void printOnConsole(std::map<std::string, int> mapCurrentRange) {
 	std::map<std::string, int>::iterator itr;
